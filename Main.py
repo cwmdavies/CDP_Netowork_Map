9b18f53eee98b8326268d7dc9bd4a519cd33484d
Christopher Davies <39084295+cwmdavies@users.noreply.github.com>
2022-06-18 09:36:30 +0100

    Minor change to docstring

--- Main.py
+++ Main.py
@@ -11,6 +11,7 @@ IP Address for each CDP neighbour and saved to a separate list. Another list is 
 of those that have been processed so no switch is connected too more than once. Each IP Address in the list
 is connected to, up to 10 at a time, to retrieve the same information. This recursion goes on until there are no
 more IP Addresses to connect to. The information is then converted to a numpy array and saved to an Excel spreadsheet.
+
 Threading is used to connect to multiple switches at a time.
 Each IP Address is checked to ensure each IP Address is valid.
 """

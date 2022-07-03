3a5106d71b3f5f29e4a80328e0f21a9c8c848380
Christopher Davies <39084295+cwmdavies@users.noreply.github.com>
2022-07-03 16:40:00 +0100

    Added Threading

--- MyPackage/MyGui.py
+++ MyPackage/MyGui.py
@@ -2,10 +2,12 @@ from tkinter import ttk, Label, Button, Entry, StringVar, filedialog
 from tkinter.messagebox import showinfo, showerror
 import sys
 import ipaddress
+from threading import Thread
 
 
-class MyGUIClass:
+class MyGUIClass(Thread):
     def __init__(self, master):
+        super().__init__()
         self.master = master
         master.title("CDP Network Map")
         master.resizable(False, True)

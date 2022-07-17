8518bf03487c83bffb73f4a4e87c683539f94395
Christopher Davies <39084295+cwmdavies@users.noreply.github.com>
2022-07-17 17:21:04 +0100

    Added Asyncssh and Asyncio test code

--- Asyncssh Test.py
+++ Asyncssh Test.py
@@ -3,41 +3,34 @@ import asyncssh
 import textfsm
 import pandas as pd
 
-jump_user = 'cwmdavies'
-jump_pass = '!Lepsodizle0!'
-switch_user = 'chris'
-switch_pass = '!Lepsodizle0!'
+jump_user = ''
+jump_pass = ''
+switch_user = ''
+switch_pass = ''
 
 jumper_server = {'host': '192.168.1.31', 'username': jump_user, 'password': jump_pass, 'known_hosts': None}
+hosts_list = ['192.168.1.2']
 
 
-async def run_client(host, username, password, command):
+async def run_client(host, command):
     async with asyncssh.connect(**jumper_server) as jump:
         async with asyncssh.connect(
                 host=host,
                 tunnel=jump,
-                username=username,
-                password=password,
+                username=switch_user,
+                password=switch_pass,
                 known_hosts=None
                 ) as connection:
             return await connection.run(command)
 
 
 async def run_multiple_clients():
+    global hosts_list
     tasks = list()
 
-    hosts_list = [
-        {
-            'host': '192.168.1.2',
-            'username': switch_user,
-            'password': switch_pass,
-            'command': 'show cdp nei detail'
-         },
-    ]
-
     for host in hosts_list:
-
-        task = run_client(host['host'], host['username'], host['password'], host['command'])
+        ip_address = host
+        task = run_client(host, 'show cdp nei detail')
         tasks.append(task)
     results = await asyncio.gather(*tasks, return_exceptions=True)
 
@@ -52,20 +45,13 @@ async def run_multiple_clients():
             print(f'Task {i} exited with status {result.exit_status}:')
             print(result.stderr, end='')
         else:  # and the else branch for the case where was neither an Exception was raised nor an error occurred.
-            # print(f'Task {i} succeeded:')
-            # print(result.stdout, end='')
-
             with open("textfsm/cisco_ios_show_cdp_neighbors_detail.textfsm") as f:
                 re_table = textfsm.TextFSM(f)
                 output = re_table.ParseText(result.stdout)
                 output = [dict(zip(re_table.header, entry)) for entry in output]
-                audit_array = pd.DataFrame(output)
-                print(audit_array)
-
-
-        # print('\n')
-        # print(50 * '#')
-
+                for entry in output:
+                    entry['LOCAL_IP'] = ip_address
+                # audit_array = pd.DataFrame(output)
+                print(output)
 
 asyncio.run(run_multiple_clients())
-

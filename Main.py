d1c4292c0362f718534d54c55d7924b337b2110a
Christopher Davies <chris.davies@weavermanor.co.uk>
2022-10-20 13:57:59 +0100

    Minor edit to docstring text

--- Main.py
+++ Main.py
@@ -7,8 +7,8 @@ Tested on Python 3.10
 This script takes in up to two IP Addresses, preferably the core switches, runs the "Show CDP Neighbors Detail"
 command and saves the information to a list of dictionaries. Each dictionary is then parsed for the neighbouring
 IP Address for each CDP neighbour and saved to a separate list. Another list is used to store the IP Addresses
-of those that have been processed so no switch is connected too more than once. Each IP Address in the list
-is connected to, up to 10 at a time, to retrieve the same information. This recursion goes on until there are no
+of those that have been processed so no switch is connected too more than once. A connection is made to each IP Address
+in the list , using threading, to retrieve the same information. This recursion goes on until there are no
 more IP Addresses to connect to. The information is then converted to a numpy array and saved to an Excel spreadsheet.
 Threading is used to connect to multiple switches at a time.
 Each IP Address is checked to ensure each IP Address is valid.
